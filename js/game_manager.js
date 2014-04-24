1927f07c1fe567d5d6ed291ff11f3a884f752f55
James <jsimpsonjr00@users.noreply.github.com>
2014-04-24 11:37:51 -0400

    added goal 2048 tile

--- js/game_manager.js
+++ js/game_manager.js
@@ -60,6 +60,8 @@ GameManager.prototype.setup = function () {
 
 // Set up the initial tiles to start the game with
 GameManager.prototype.addStartTiles = function () {
+  var goalTile = new Tile( this.grid.randomAvailableCell(), 2048 );
+  
   for (var i = 0; i < this.startTiles; i++) {
     this.addRandomTile();
   }
